56bcc9ca686de189ec92c226ad3e9a186808a788
Mordecai <edwincesaryauyosoto@gmail.com>
2020-02-04 15:07:27 -0500

    traslado de camara
    Co-Authored-By: Piero Violeta Estrella <pierovioleta@users.noreply.github.com>

--- Operations for kids/Assets/Scripts/cameraMov.cs
+++ Operations for kids/Assets/Scripts/cameraMov.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class cameraMov : MonoBehaviour
 {
+    public GameObject cam;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+   
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount==1 && cam.transform.position==new Vector3(0,0,-10))
+        {
+            cam.transform.position = new Vector3(20, 0, -10);
+;       }
     }
 }
