6122138c6c53dfd0e5cbe1a555fe427bc386d139
Mordecai <edwincesaryauyosoto@gmail.com>
2020-02-05 14:58:12 -0500

    mmmmm

---
+++ Operations for kids/Assets/Scripts/Obj.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obj : MonoBehaviour
+{
+    public GameObject obj1;
+    public GameObject obj2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("obj1"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            transform.position=obj1.transform.position;
+           
+
+        }
+
+
+        else if (col.gameObject.CompareTag("obj2"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            transform.position=obj2.transform.position;
+            
+
+        }
+        
+    }
+}
