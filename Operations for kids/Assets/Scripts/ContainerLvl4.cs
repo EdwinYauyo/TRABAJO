11067c645a950a7aaf6f022942fd8d1eb155d76b
PieroVioleta <piero759@gmail.com>
2020-02-05 20:16:01 -0500

    Lvl4 Added

---
+++ Operations for kids/Assets/Scripts/ContainerLvl4.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContainerLvl4 : MonoBehaviour
+{
+    public GameObject ballie, cesta;
+    public int n1 = 8;
+    public int n2 = 6; //como maximo 5
+    void Start()
+    {
+        for (int i = 0; i < n1; i++) {
+            Instantiate(ballie, new Vector3(38f + (i + 1)*6/n1 , 4, -1), Quaternion.identity);
+        }
+        for (int i = 0; i < n2; i++)
+        {
+            Instantiate(cesta, new Vector3(31f + (i+1)*14f / (n2), -4.5f, -2), Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Operations for kids/Assets/Scripts/DeleteBallLvl4.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteBallLvl4 : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("ball")) {
+            Debug.Log("xd");
+            Destroy(col.gameObject, 0f);
+        }
+    }
+}
